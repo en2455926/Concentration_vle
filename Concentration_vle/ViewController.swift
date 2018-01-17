ae924ac00a5fc8eb220401b77f1345dec0fc8c67
en2455926 <en2455926@gmail.com>
2018-01-17 12:26:38 +0200

    Initial Commit

---
+++ Concentration_vle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration_vle
+//
+//  Created by Koulutus on 17/01/2018.
+//  Copyright © 2018 Vesku L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
